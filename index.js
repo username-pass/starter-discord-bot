833a572bfdd88b46ae10d79e56bc664381d7685b
Loading <98624927+username-pass@users.noreply.github.com>
2023-02-13 15:45:25 -0800

    changed things

--- index.js
+++ index.js
@@ -15,118 +15,13 @@ console.clear();
 
 //og code START
 client.on("message", msg => {
+	let username = msg.author.id;
+	let content = msg.content;
+	let mentions = msg.mentions.users.entries()
+				
+	 msg.channel.send('username'+username+': content'+content+'mentions: '+mentions);
+
 	
-	if (msg.content.toLowerCase() == prefix.toLowerCase() + "logchannel") {
-		msg.channel.send("Channel logged to console.");
-		console.log(msg.channel)
-	} else if (msg.content.toLowerCase().includes(prefix.toLowerCase() + "debug")) {
-		msg.channel.send("Message logged to console.")
-		console.log(msg);
-	} else if (msg.content.toLowerCase() == prefix.toLowerCase() + "welcome") {
-		if (JSON.parse(fs.readFileSync('info.json'))["admins"].includes(msg.author.id)) {
-
-			msg.channel.send("Hello, Im iຖวē¢t0r, the bot! my job is to interface with discord and allow my maker, littleclaw, the power to control his project from discord")
-
-		} else {
-			msg.channel.send("[inject0r] You are not an admin!")
-		}
-
-	} else if (msg.content.toLowerCase().includes(prefix.toLowerCase() + "createregcode")) {
-		if (JSON.parse(fs.readFileSync('info.json'))["admins"].includes(msg.author.id)) {
-			if (msg.mentions.users.size == 0) {
-				msg.channel.send('[inject0r] You have to mention a user!')
-			} else {
-				msg.channel.send("[inject0r] Fetching a new auth token from server...");
-				let mentions = msg.mentions.users.entries()
-				async function getAKey() {
-					let key = await fetch('https://inject0r.littleclaw.repl.co/token', {
-						method: 'GET',
-						headers: {
-							'token': authtoken
-						}
-					})
-					return await key.text();
-				};
-
-				getAKey().then(recievedToken => {
-					try {
-						let embed = new Discord.MessageEmbed()
-						embed.setTitle('Registration token!')
-						embed.setColor('#0053f6')
-						embed.setURL('https://inject0r.littleclaw.repl.co/register')
-						embed.setAuthor('Thanks For Joining Us At Inject0r', 'https://inject0r.littleclaw.repl.co/logo.png');
-						embed.setDescription('Your registration token has been created! Find the current bookmark when you register below.');
-						embed.addFields(
-							{ name: 'Your registration token is:', value: recievedToken },
-							{ name: 'Register at:', value: 'https://inject0r.littleclaw.repl.co/register' },
-						)
-						embed.setThumbnail('https://inject0r.littleclaw.repl.co/logo.png')
-						embed.setTimestamp();
-						embed.setFooter('Bot created by littleclaw', 'https://inject0r.littleclaw.repl.co/logo.png')
-						mentions.next().value[1].send(embed);
-						//#0053f6
-
-						msg.channel.send("[inject0r] Registration token successfully sent to user!");
-					} catch (err) {
-						msg.channel.send("An error occured, and the registration token could not be sent. Error logged to Console.");
-						console.log(err);
-					}
-				});
-
-			}
-		} else {
-			msg.channel.send("[inject0r] You are not an admin!")
-		}
-	}
-
-	else if (msg.content.toLowerCase().includes(prefix.toLowerCase() + "recruit")) {
-		if (JSON.parse(fs.readFileSync('info.json'))["admins"].includes(msg.author.id)) {
-			if (msg.mentions.users.size == 0) {
-				msg.channel.send('[inject0r] You have to mention a user to advertise to!')
-
-			} else {
-				msg.channel.send("[inject0r] Fetching a new auth token from server...");
-				let mentions = msg.mentions.users.entries()
-				async function getAKey() {
-					let key = await fetch('https://inject0r.littleclaw.repl.co/token', {
-						method: 'GET',
-						headers: {
-							'token': authtoken
-						}
-					})
-					return await key.text();
-				};
-
-				getAKey().then(recievedToken => {
-					try {
-						let embed = new Discord.MessageEmbed()
-						embed.setTitle('Important message:')
-						embed.setColor('#f60014')
-						embed.setURL('https://inject0r.littleclaw.repl.co/')
-						embed.setAuthor('Hey, you have been selected to join a beta test!!', 'https://inject0r.littleclaw.repl.co/logo.png');
-						embed.setDescription('You have been selected as one of a chosen few to participate in a closed-beta testing of something littleclaw has made. If you wish to join, than take the registeration token below and go to the link at the bottom of this message. Sign up and start your exclusive beta-testing access!!!');
-						embed.addFields(
-							{ name: 'Your one time use registration token is:', value: recievedToken },
-							{ name: 'Register below at:', value: 'https://inject0r.littleclaw.repl.co/register' },
-						)
-						embed.setThumbnail('https://inject0r.littleclaw.repl.co/logo.png')
-						embed.setTimestamp();
-						embed.setFooter('who knows what the sniper monkeys think of...', 'https://inject0r.littleclaw.repl.co/logo.png')
-						mentions.next().value[1].send(embed);
-						//#0053f6
-
-						msg.channel.send("[inject0r] Recruit message and registration token successfully sent to target user!");
-					} catch (err) {
-						msg.channel.send("An error occured, and the message and token could not be sent. Error probally was logged to Console.");
-						console.log(err);
-					}
-				});
-
-			}
-		} else {
-			msg.channel.send("[inject0r] You are not an admin!")
-		}
-	}
 })
 client.on('message', message => {
 	if (message.content.includes('[inject0r]', 'inj:')) {
