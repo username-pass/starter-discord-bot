1825accf8068c9d41d125a820b53398155935db2
Loading <98624927+username-pass@users.noreply.github.com>
2023-02-13 16:25:09 -0800

    random code

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
@@ -12,11 +6,14 @@ client.on('ready',() => {
     console.log('FBB Online!');
 });
 
-client.on("message", msg => {
-	let username = msg.author.id;
+client.on('message', msg => {
+    if (msg.author === client.user) return;
+    let username = msg.author.id;
 	let content = msg.content;
 	let mentions = msg.mentions.users.entries()
+				
 	 msg.channel.send('username'+username+': content'+content+'mentions: '+mentions);
-})
+
+});
 
 client.login(settings.token);
